66cff4c83e3ec0a269359e434a35bb612d71b88e
Ömer Gürbüz <mhmd.omer.grbz@gmail.com>
2022-03-14 18:52:12 +0300

    ORM added but not working

---
+++ LibrarySimulation/Data/BookDbContext.cs
@@ -0,0 +1,30 @@
+﻿using LibrarySimulation.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySimulation.Data
+{
+    class BookDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\Mssqllocaldb; Database=LibrarySimulatorDb; Integrated security=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(x => x.Name)
+                                       .IsRequired()
+                                       .HasMaxLength(250);
+        }
+    }
+}
---
+++ LibrarySimulation/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySimulation.Models
+{
+    class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int PageCount { get; set; }
+        public DateTime Year { get; set; }
+        public bool IsInShelf { get; set; }
+
+
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+
+
+        public List<User> Users { get; set; }
+
+
+        public List<Category> Categories { get; set; }
+    }
+}
